fff2b07690dc247f59b7375d0751fcf6d9431b8e
Aleksandr16333 <97799611+Aleksandr16333@users.noreply.github.com>
2022-02-19 20:09:32 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int amount = 10000;
+        int bonus = amount / 20;
+        int mile = bonus * 1;
+
+        System.out.println("Ваши мили " + mile);
+
+    }
+}
